64923cdce137130b6ef4cac47e532d108b599ea3
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2021-11-30 22:28:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -224,6 +224,47 @@ btnSort.addEventListener('click', function (e) {
   sorted = !sorted;
 });
 
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      //cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+console.log(deposits, withdrawals);
+
+const convertTitleCase = function (title) {
+  const exeption = ['a', 'is', 'an', 'the', 'but', 'or', 'on', 'in', 'with'];
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word =>
+      exeption.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(' ');
+  //console.log(titleCase);
+  return titleCase;
+};
+
+const convert = function (title) {
+  const ex = ['an', 'is', 'the', 'a', 'and'];
+
+  const wrd = title
+    .toLowerCase()
+    .split(' ')
+    .map(word =>
+      ex.includes(word) ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(' ');
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
 /////////////////////////
 /*
 const bankDepositSum = accounts.map(acc => acc.movements).flat(); // all element from other arrays in one
@@ -244,8 +285,13 @@ console.log(summa); // summa of all values
 
 const bankDeposit1000 = accounts
   .flatMap(acc => acc.movements)
-  .filter(mov => mov > 1000);
+  .filter(mov => mov >= 1000).length; // 6 elements is more then 1000
 console.log(bankDeposit1000);
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+console.log(numDeposits1000);
 
 /////////////////////////////////////
 /*
@@ -638,4 +684,32 @@ console.log(accounts);
 
 const account = accounts.find(accoun => accoun.owner === 'Jessica Davis');
 console.log(account);
+*/
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+GOOD LUCK 😀
 */
